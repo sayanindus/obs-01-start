551388531bb1a577ac10e1ad9ddb1a180bf0812b
Sayan Dey <sayantadeytraining@gmail.com>
2020-02-10 22:36:52 +0530

    Error & completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,11 +21,21 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if ( count > 3 ) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
     this.firstObservable = customObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('Completed');
     });
   }
   
